5fa2f5542e9f1414a842619b7ab21726c672df3d
ted <theo_drz@live.com>
2022-02-09 13:48:13 -0500

    working grid function and basic hover effect

--- script1.js
+++ script1.js
@@ -2,14 +2,16 @@ console.log("START");
 
 const containerVar = document.querySelector("#container");
 
+
+ // TURN THIS INTO A FUNCTION LIKE makeGrid(x,y)
 let y = 0
-while (y < 5) {
+while (y < 0) {
     //console.log("x is "+x);
     const itemCont = document.createElement("div");
     itemCont.classList.add("itemCont");
     
     let x = 0;
-    while (x < 4) {
+    while (x < 0) {
         const item = document.createElement("div");
         item.classList.add("sqrs")
 
@@ -19,4 +21,26 @@ while (y < 5) {
     containerVar.appendChild(itemCont);
     y++;
 }
-  
+
+function makeGrid(rowTotal, columnTotal) {
+    //console.log(rowTotal + " and " + columnTotal)
+    let columnCount = 0
+    while (columnCount < columnTotal) {
+        const itemCont = document.createElement("div");
+        itemCont.classList.add("itemCont");
+
+        let rowCount = 0
+        while (rowCount < rowTotal) {
+            const item = document.createElement("div");
+            item.classList.add("sqrs");
+
+            itemCont.appendChild(item);
+            rowCount++
+        }
+
+        containerVar.appendChild(itemCont);
+        columnCount++
+    }
+}
+
+makeGrid(8,8)
